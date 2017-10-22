6775ca918def403098ee442bfdb1966f523cdd9d
Jyotirmoy Roy <jroy2516@uni.sydney.edu.au>
2017-10-23 02:56:14 +1100

    displays specific events and also added firebase support for the events

--- app/src/main/java/com/google/firebase/udacity/friendlychat/EventsActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/EventsActivity.java
@@ -1,8 +1,17 @@
 package com.google.firebase.udacity.friendlychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +29,36 @@ public class EventsActivity extends AppCompatActivity {
 
         mEventsListView = (ListView) findViewById(R.id.listview);
 
-        List<Event> eventsList = new ArrayList<>();
-        eventsList.add(new Event("Party at Manning", "Manning Bar", "Drinks on house", "Party"));
-        eventsList.add(new Event("Trivia Night", "Manning Bar", "Drinks on house", "Party"));
-
+        final List<Event> eventsList = new ArrayList<>();
         mEventAdapter = new EventAdapter(this, R.layout.item_event, eventsList);
         mEventsListView.setAdapter(mEventAdapter);
-        
+
+
+        DatabaseReference mEventsReference = FirebaseDatabase.getInstance().getReference().child("Events");
+        mEventsReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot event: dataSnapshot.getChildren()){
+                    Event tempEvent = event.getValue(Event.class);
+                    mEventAdapter.add(tempEvent);
+                    mEventAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mEventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Event event = eventsList.get(position);
+                Intent intent = new Intent(EventsActivity.this, SpecificEventActivity.class);
+                intent.putExtra("event", event);
+                startActivity(intent);
+            }
+        });
     }
 }
